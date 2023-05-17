a206be7cd5478cee8271480422b151047e4befa3
Ilia Shevchenko <sheff.ilya2010@gmail.com>
2023-05-17 15:30:44 +0300

    The third task

--- CodeWars.js
+++ CodeWars.js
@@ -6,6 +6,10 @@ function hero(bullets, dragons){
       return false;
     }
 }
+let bullets = 8;
+let dragons = 4;
+hero(bullets, dragons);
+
 
 //The second task.
 function points(games) {
@@ -19,5 +23,23 @@ function points(games) {
   }
   return result;
 }
+let games = ["1:0","2:0","3:0","4:0","2:1","3:1","4:1","3:2","4:2","4:3"];
+points(games);
 
+
+//The third task.
+function updateLight(current) {
+  if (current === "green") {
+    current = "yellow";
+    return current;
+  } else if (current === "yellow") {
+    current = "red";
+    return current;
+  } else if (current === "red") {
+    current = "green";
+    return current;
+  }
+}
+let current = "green";
+updateLight(current);
 
