c9ba83ccf3205929c6dda8a758793b19f6d76f83
Ilia Shevchenko <sheff.ilya2010@gmail.com>
2023-05-18 14:31:58 +0300

    The eigth task

--- CodeWars.js
+++ CodeWars.js
@@ -81,3 +81,16 @@ function evenOrOdd(number) {
   }
 }
 evenOrOdd(7);
+
+
+//The eigth task.
+function positiveSum(arr) {
+  let result = 0;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] > 0) {
+      result += arr[i];
+    }
+  }
+  return result;
+}
+positiveSum([1, 2, 3, 4, 5]);
